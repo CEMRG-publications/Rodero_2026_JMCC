50786c967378fe2d11a522d009efe9a8ef11c110
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2024-08-06 12:18:43 +0100

    first commit

---
+++ simulation_toolbox/write_unloading_scripts.py
@@ -0,0 +1,141 @@
+import os
+import sys
+
+import numpy as np
+import json
+import copy
+import argparse
+
+from UNLOADING_library.headers import hpc_headers
+
+def main(args):
+
+    print("Generating unloading scripts from "+args.paramfolder+"...")
+
+    if not os.path.exists(args.slrmfolder):
+      os.system("mkdir -p "+args.slrmfolder)
+
+    idx_1 = int(args.idx1)
+    idx_2 = int(args.idx2)
+
+    input_params = ["a_ventricles",
+                    "bf_ventricles",
+                    "bfs_ventricles",
+                    "bt_ventricles",
+                    "a_atria",
+                    "bf_atria",
+                    "bfs_atria",
+                    "bt_atria",
+                    "k_peri",
+                    "EDP_lv",
+                    "EDP_rv",
+                    "a_lvrv",
+                    "bf_lv_scaling",
+                    "bf_rv_scaling",
+                    "bf_aa_scaling"]
+
+    settings_file = args.setup_file
+    with open(settings_file,"r") as f:
+        settings = json.load(f)
+
+    ncores = int(settings["nproc"])
+    settings["walltime"] = "02:00:00"
+
+    path2unloading = args.HPC_path_to_unloading_library
+    path2carputils = args.HPC_path_to_carputils
+
+    carp_config_file    = args.HPC_path_to_CARP_config
+    archer2_config_file = None
+
+    env_folder = args.HPC_env_folder
+
+    env_variabiles = hpc_headers.write_env_variables(path2unloading,
+                                                     path2carputils,
+                                                     carp_config_file,
+                                                     ncores,
+                                                     env_folder,
+                                                     archer2_config_file)
+
+    print("Saving slrm files to "+args.slrmfolder+"...")
+    for i in range(idx_1,idx_2+1):
+        with open(os.path.join(args.paramfolder,str(i)+".json"),"r") as f:
+          parameters = json.load(f)
+
+        output_basename = "unloading_"+str(i)
+
+        header = hpc_headers.write_archer2_header(output_basename,
+                                                  output_basename+".out",
+                                                  settings["walltime"],
+                                                  ncores) 
+
+        runcommand = ["python",args.python_script_path_archer2,"--platform","desktop"]
+        runcommand += ["--overwrite-behaviour","overwrite"]
+        runcommand += ["--np",str(ncores)]
+        runcommand += ["--testname",output_basename]        
+
+        runcommand += ["--tags_setup_file",args.HPC_tags_file]
+        runcommand += ["--general_setup_file",args.HPC_setup_file]      
+
+        for ip in input_params:
+          runcommand += ["--"+ip,str(parameters["mechanics"][ip])]        
+
+        runcommand = ' '.join(runcommand) 
+
+        # -------------------------------------
+        # write slrm file 
+        slrm_script = os.path.join(args.slrmfolder,output_basename+".slrm")
+        f = open(slrm_script,"w")       
+
+        f.write(header)
+        f.write(env_variabiles)
+        f.write(runcommand)       
+
+        f.close()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
+
+    parser.add_argument('--datafolder', type=str, required=True,
+                        help='Provide folder where you have all X_*.txt and xlabels_*.txt')
+
+    parser.add_argument('--setup_file', type=str, required=True,
+                        help='.json file with the setup for the simulations')  
+
+    parser.add_argument('--paramfolder', type=str, required=True,
+                        help='Where to save the json parameter files')
+
+    parser.add_argument('--slrmfolder', type=str, required=True,
+                        help='Where to save the slrm files')  
+
+    parser.add_argument('--idx1', type=str, required=True, 
+                        help='First index to generate the files for')  
+
+    parser.add_argument('--idx2', type=str, required=True,
+                        help='Last index to generate the files for')  
+
+    parser.add_argument('--HPC_tags_file', type=str, required=True,
+                        help='Json file with the tags.')  
+
+    parser.add_argument('--HPC_setup_file', type=str, required=True,
+                        help='Json file with the tags.')  
+
+    parser.add_argument('--HPC_path_to_unloading_library', type=str, required=True,
+                        help='Path to the unloading library on the HPC')  
+
+    parser.add_argument('--HPC_path_to_carputils', type=str, required=False,
+                        default="/work/e348/e348/shared/carputils/",
+                        help='Path to the carputils on the HPC')  
+
+    parser.add_argument('--HPC_path_to_CARP_config', type=str, required=False,
+                        default="/work/e348/e348/shared/software/carpentry-system-petsc/carp.conf",
+                        help='Path to the carp.conf file on the HPC')  
+
+    parser.add_argument('--HPC_env_folder', type=str, required=True,
+                        help='Path to the pyunload virtual environment on the HPC')  
+    parser.add_argument('--python_script_path_archer2', type=str, required=True)
+
+    args = parser.parse_args()
+
+    main(args)
