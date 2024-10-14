7e6b56d5be96b3d71a57db86917c0c8ec03baa73
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2024-10-14 14:14:56 +0100

    adapted to take in the default simulation

--- simulation_toolbox/check_unloading_convergence_archer2.py
+++ simulation_toolbox/check_unloading_convergence_archer2.py
@@ -12,62 +12,85 @@ def file_exists(full_file_path):
 
 
 def check_fourchamber_unloading_(simulation_folder,
-								 chambers):
+                                 chambers):
 
-	converged_1 = False
-	with open(simulation_folder+"unloading.log", 'r') as file:
-		content = file.read()
-		if "--- CONVERGED ---" in content:
-			converged_1 = True
+    converged_1 = False
+    with open(simulation_folder+"unloading.log", 'r') as file:
+        content = file.read()
+        if "--- CONVERGED ---" in content:
+            converged_1 = True
 
-	converged_2 = False
-	if not os.path.exists(simulation_folder+"temp/"):
-		converged_2 = True
+    converged_2 = False
+    if not os.path.exists(simulation_folder+"temp/"):
+        converged_2 = True
 
-	converged = False
-	if converged_1 and converged_2:
-		converged = True
+    converged = False
+    if converged_1 and converged_2:
+        converged = True
 
-	output = [converged]
-	volumes = []
-	for ch in chambers:
-		if converged and os.path.exists(simulation_folder+"final_data/"+ch+".vol.dat"):
-			vol = np.loadtxt(simulation_folder+"final_data/"+ch+".vol.dat",dtype=float,usecols=[1])
-			output.append(vol[0])
-		else:
-			output.append(-1)
+    output = [converged]
+    volumes = []
+    for ch in chambers:
+        if converged and os.path.exists(simulation_folder+"final_data/"+ch+".vol.dat"):
+            vol = np.loadtxt(simulation_folder+"final_data/"+ch+".vol.dat",dtype=float,usecols=[1])
+            output.append(vol[0])
+        else:
+            output.append(-1)
 
-	return output
+    return output
 
 def check_fourchamber_unloading(basefolder,
-								chambers,
-								start_sample=0,
-								last_sample=1,
-								output_file='unloaded_volumes.txt'):
+                                chambers,
+                                start_sample,
+                                last_sample,
+                                output_file='unloaded_volumes.txt'):
 
-	vol_unloaded = np.zeros((last_sample-start_sample+1,len(chambers)),dtype=float)
-	count = 0
+    # vol_unloaded = np.zeros((last_sample-start_sample+1,len(chambers)),dtype=float)
+    # count = 0
+    if start_sample is not None:
+        vol_unloaded = -1*np.ones((last_sample+1,len(chambers)),dtype=float)
 
-	for i in range(start_sample,last_sample+1):	
+        for i in range(start_sample,last_sample+1):    
 
-		output = check_fourchamber_unloading_(basefolder+'/unloading_'+str(i)+'/',
-											  chambers)
+            output = check_fourchamber_unloading_(basefolder+'/unloading_'+str(i)+'/',
+                                                chambers)
 
-		if output[0]:
-			print('unloading_'+str(i)+' successful...')
+            if output[0]:
+                print('unloading_'+str(i)+' successful...')
 
-			for j in range(len(chambers)):
-				vol_unloaded[count,j] = output[1+j]
+                for j in range(len(chambers)):
+                    # vol_unloaded[count,j] = output[1+j]
 
-		else:
-			print('unloading_'+str(i)+' crashed...')
-			
-			for j in range(len(chambers)):
-				vol_unloaded[count,j] = -1
+                    vol_unloaded[i,j] = output[1+j]
 
-		count += 1
+            else:
+                print('unloading_'+str(i)+' crashed...')
+                
+                for j in range(len(chambers)):
+                    # vol_unloaded[count,j] = -1
+                    vol_unloaded[i,j] = -1
 
-	np.savetxt(output_file,vol_unloaded,fmt="%g")
+    else:
+        vol_unloaded = -1*np.ones((2,len(chambers)),dtype=float)
+
+        output = check_fourchamber_unloading_(f"{basefolder}/unloading_default/",chambers)
+        if output[0]:
+            print('unloading_default successful...')
+
+            for j in range(len(chambers)):
+                # vol_unloaded[count,j] = output[1+j]
+
+                vol_unloaded[0,j] = output[1+j]
+
+        else:
+            print('unloading_default crashed...')
+            
+            for j in range(len(chambers)):
+                # vol_unloaded[count,j] = -1
+                vol_unloaded[0,j] = -1
+
+
+    np.savetxt(output_file,vol_unloaded,fmt="%g")
 
 def main(args):
 
@@ -75,58 +98,61 @@ def main(args):
     path2figure = args.path2figures
     first_simulation = args.first_simulation
     last_simulation = args.last_simulation
+    default  = args.default
 
-    file_exists(os.path.join(basefolder,"../data/X_mechanics.txt"))
-    file_exists(os.path.join(basefolder,"../data/xlabels_mechanics.txt"))
+    if not default:
+        file_exists(os.path.join(basefolder,"../data/X_mechanics.txt"))
+        file_exists(os.path.join(basefolder,"../data/xlabels_mechanics.txt"))
 
     os.system("mkdir -p "+ os.path.join(basefolder,"unloaded/"))
     os.system("mkdir -p "+ os.path.join(basefolder,"../figures/"))
 
     check_fourchamber_unloading(basefolder,
-							['lv_endo','rv_endo','la_endo','ra_endo'],
-							start_sample=first_simulation,
-							last_sample=last_simulation,
-							output_file=os.path.join(basefolder,'unloaded_volumes.txt'))    
+                            ['lv_endo','rv_endo','la_endo','ra_endo'],
+                            start_sample=first_simulation,
+                            last_sample=last_simulation,
+                            output_file=os.path.join(basefolder,'unloaded_volumes.txt'))    
     
-    unloaded_volumes = np.loadtxt(os.path.join(basefolder,"unloaded_volumes.txt"),dtype=float)
+    if not default:
+        unloaded_volumes = np.loadtxt(os.path.join(basefolder,"unloaded_volumes.txt"),dtype=float)
 
-    X = np.loadtxt(os.path.join(basefolder,"../data/X_mechanics.txt"),dtype=float)
-    if type(np.size(unloaded_volumes)) is not int :
-        mask = np.sum(unloaded_volumes,axis=1)
-    else:
-        mask = np.sum(unloaded_volumes)
+        X = np.loadtxt(os.path.join(basefolder,"../data/X_mechanics.txt"),dtype=float)
+        if type(np.size(unloaded_volumes)) is not int :
+            mask = np.sum(unloaded_volumes,axis=1)
+        else:
+            mask = np.sum(unloaded_volumes)
 
-    xlabels = read_labels(os.path.join(basefolder,"../data/xlabels_mechanics.txt"))
+        xlabels = read_labels(os.path.join(basefolder,"../data/xlabels_mechanics.txt"))
 
-    idx_ok = np.where(mask!=0)[0]
-    idx_notok = np.where(mask==0)[0]
+        idx_ok = np.where(mask!=0)[0]
+        idx_notok = np.where(mask==0)[0]
 
-    if len(X.shape) > 1:
-        in_dim = X.shape[1]
-		
-        out_dim = in_dim
-        _, axes = plt.subplots(
-            nrows=out_dim,
-            ncols=in_dim,
-            sharex="col",
-            sharey="row",
-            figsize=(10,10),
-        )
-        for i, axis in enumerate(axes.flatten()):
-            axis.scatter(X[idx_ok, i % in_dim], X[idx_ok, i // in_dim], c='green', s=1)
-            axis.scatter(X[idx_notok, i % in_dim], X[idx_notok, i // in_dim], c='red', s=1)
-            inf = min(X[:, i % in_dim])
-            sup = max(X[:, i % in_dim])
-            mean = 0.5 * (inf + sup)
-            delta = sup - mean
-            if i // in_dim == out_dim - 1:
-                axis.set_xlabel(xlabels[i % in_dim],rotation=90)
-                axis.set_xticks([])
-                axis.set_xlim(left=inf - 0.3 * delta, right=sup + 0.3 * delta)
-            if i % in_dim == 0:
-                axis.set_yticks([])
-                axis.set_ylabel(xlabels[i // in_dim])
-        plt.savefig(os.path.join(path2figure, "unloaded_scatter.png"), bbox_inches="tight", dpi=300)
+        if len(X.shape) > 1:
+            in_dim = X.shape[1]
+            
+            out_dim = in_dim
+            _, axes = plt.subplots(
+                nrows=out_dim,
+                ncols=in_dim,
+                sharex="col",
+                sharey="row",
+                figsize=(10,10),
+            )
+            for i, axis in enumerate(axes.flatten()):
+                axis.scatter(X[idx_ok, i % in_dim], X[idx_ok, i // in_dim], c='green', s=1)
+                axis.scatter(X[idx_notok, i % in_dim], X[idx_notok, i // in_dim], c='red', s=1)
+                inf = min(X[:, i % in_dim])
+                sup = max(X[:, i % in_dim])
+                mean = 0.5 * (inf + sup)
+                delta = sup - mean
+                if i // in_dim == out_dim - 1:
+                    axis.set_xlabel(xlabels[i % in_dim],rotation=90)
+                    axis.set_xticks([])
+                    axis.set_xlim(left=inf - 0.3 * delta, right=sup + 0.3 * delta)
+                if i % in_dim == 0:
+                    axis.set_yticks([])
+                    axis.set_ylabel(xlabels[i // in_dim])
+            plt.savefig(os.path.join(path2figure, "unloaded_scatter.png"), bbox_inches="tight", dpi=300)
 
 
 if __name__ == '__main__':
@@ -137,9 +163,9 @@ if __name__ == '__main__':
     parser.add_argument('--path2simulations', type=str, required=True,
                         default="/media/croderog/SeagateExpansionDrive/h01/new_unloading/unloading_simulations")
     parser.add_argument('--path2figures', type=str, required=True)
-    parser.add_argument('--first_simulation', type=int, required=False, default=0)
-    parser.add_argument('--last_simulation', type=int, required=False, default=99)
-
+    parser.add_argument('--first_simulation', type=int, required=False)
+    parser.add_argument('--last_simulation', type=int, required=False)
+    parser.add_argument('--default', action='store_true')
     args = parser.parse_args()
 
     main(args)
